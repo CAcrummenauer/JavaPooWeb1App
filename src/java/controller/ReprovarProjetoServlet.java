969377a95bc3ac4485ca79d75fa4e3f5b578088e
Cezar Augusto Crummenauer <cezar@DESKTOP-7F7HMLP>
2018-11-22 14:20:26 -0200

    Aprovar e reprovar projetos ok v3...

---
+++ src/java/controller/ReprovarProjetoServlet.java
@@ -0,0 +1,30 @@
+package controller;
+
+import dao.ProjetoDao;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "reprovarProjeto")
+public class ReprovarProjetoServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        int id = Integer.parseInt(httpServletRequest.getParameter("id"));
+        new ProjetoDao().reprovarProjeto(id);
+        RequestDispatcher requestDispatcher;
+        requestDispatcher = httpServletRequest.getRequestDispatcher("projetos.jsp");
+        requestDispatcher.forward(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher;
+        requestDispatcher = httpServletRequest.getRequestDispatcher("projetos.jsp");
+        requestDispatcher.forward(httpServletRequest, httpServletResponse);
+    }
+}
